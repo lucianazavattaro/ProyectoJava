9e7f5ac9f2e3bc6c97b24e7a8349122fc2a2ea8b
lucianazavattaro <luliana93@outlook.com>
2020-06-15 19:46:55 -0300

    "Primer commit"

---
+++ Untitled-1.js
@@ -0,0 +1,13 @@
+let typeCard: "debit card"
+
+switch (typeCard) {
+    case "debit card"
+        console.log ("Tarjeta de debito");
+        break;
+    case "credit card":
+        console.log("tarjeta de credito");
+        breaks;
+    default:
+        console.log ("No se reconoce tarjeta");    
+
+}
